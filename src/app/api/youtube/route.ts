a43d3dbc7dc05212f9a88c45108c7065982865c0
Lucas Saliés Brum <sistematico@gmail.com>
2025-11-19 07:48:57 -0400

    refactor: update output handling in YouTube download API and clean up commented code

--- src/app/api/youtube/route.ts
+++ src/app/api/youtube/route.ts
@@ -5,9 +5,7 @@ import fs from "node:fs";
 
 export async function POST(request: Request) {
   const { url } = await request.json();
-  if (!url) {
-    return NextResponse.json({ error: "URL não fornecida." }, { status: 400 });
-  }
+  if (!url) return NextResponse.json({ error: "URL não fornecida." }, { status: 400 });
 
   try {
     const outputDir = path.join(process.cwd(), "public", "music");
@@ -19,16 +17,32 @@ export async function POST(request: Request) {
       ffmpegPath: process.env.FFMPEG_PATH || "/usr/local/bin/ffmpeg",
     });
     // Gera nome único para evitar conflitos
-    const fileName = `music_${Date.now()}.mp3`;
-    const outputPath = path.join(outputDir, fileName);
+    // const fileName = `music_${Date.now()}.mp3`;
+    // const outputPath = path.join(outputDir, fileName);
 
     await ytdlp.downloadAsync(url, {
       // cookies: process.env.YTDLP_COOKIES_PATH || undefined,
-      output: outputPath,
-      format: "mp3",
+      output: outputDir + "/%(title)s.%(ext)s",
+      extractAudio: true,
+      // outputNaPlaceholder: "%(title)s.%(ext)s",
+
+      embedMetadata: true,
+      audioFormat: "mp3",
+      // format: "mp3",
     });
 
-    return NextResponse.json({ success: true, file: `/music/${fileName}` });
+    // yt-dlp -i -c -x \
+    // $COOKIES \
+    // -f bestaudio \
+    // --extract-audio \
+    // --audio-format mp3 \
+    // --embed-thumbnail \
+    // --add-metadata \
+    // -o "%(artist)s - %(title)s.%(ext)s" \
+    // "$url"
+
+
+    return NextResponse.json({ success: true });
   } catch (error) {
     const message = error instanceof Error ? error.message : String(error);
     return NextResponse.json({ error: message }, { status: 500 });
