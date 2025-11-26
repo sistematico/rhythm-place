cee4e02f4663af3436dcf0e4d460e5755dda4ef4
Lucas Saliés Brum <sistematico@gmail.com>
2025-11-26 03:04:21 -0400

    feat: Update Vite configuration to support environment variables and dynamic server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { nitro } from 'nitro/vite'
@@ -5,8 +6,10 @@ import viteReact from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
+const dev = process.env.NODE_ENV !== 'production'
+
 export default defineConfig({
-	server: { port: 3000 },
+	server: { port: dev ? 3000 : 4060 },
 	plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), nitro(), viteReact()],
 	nitro: {}
 })
